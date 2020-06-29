f1ca3f9d5dfc085a9822f33463cb82f0bbda87ed
bgnxmx <bgnxmx@gmail.com>
2020-06-30 01:00:52 +0300

    performance examples

--- client.js
+++ client.js
@@ -34,17 +34,17 @@ for (let i = 0; i < 1000; i++) {
 let newTodoText = ``;
 
 let Counter = class extends React.Component {
-  count = 0;
+  state = {
+    count: 0
+  }
   render() {
     return x({
       marginLeft: `10px`,
       marginRight: `10px`,
-      children: `count: ${this.count}`,
+      children: `count: ${this.state.count}`,
     }, {
       onClick: () => {
-        this.count += 1;
-        console.log(`count:`, this.count);
-        rerender();
+        this.setState({ count: this.state.count + 1 });
       }
     })
   }
@@ -94,59 +94,69 @@ let Todo = class extends React.Component {
   }
 }
 
-let App = () => {
-  return x({
-    flex: 1,
-    overflow: `scroll`,
-    padding: `50px`,
-    children: [
-      x({
-        alignItems: `center`,
-        fontSize: `20px`,
-        children: `Todos`
-      }),
-      x({
-        marginTop: `15px`,
-        flexDirection: `row`,
-        children: [
-          Input({
-            flex: `1`,
-            border: `1px solid gray`
-          }, { value: newTodoText, onChange: (e) => { newTodoText = e.target.value; rerender(); } }),
-          x({
-            tag: `button`,
-            marginLeft: `10px`,
-            children: `add todo`,
-          }, {
-            onClick: () => {
-              todos.push({ id: todos.length, text: newTodoText, creationTime: Math.random(), count: 0 });
-              newTodoText = ``;
-              rerender();
-            }
-          })
-        ]
-      }),
-      x({
-        marginTop: `15px`,
-        keyed: true,
-        children: todos.map((todo, i) => x({ tag: Todo }, { todo: todo, key: todo.id }))
-      }),
-      x({
-        marginTop: `10px`,
-        children: [
-          x({
-            children: [
-              x({ children: `sort by creation time` }),
-              x({
-                children: todos.slice().sort((a, b) => a.creationTime - b.creationTime).map(todo => x({ tag: Todo }, { todo: todo, key: todo.id }))
-              })
-            ]
-          })
-        ]
-      })
-    ]
-  });
-};
+let App = class extends React.Component {
+  state = {
+    count: 0
+  }
+  render() {
+    return x({
+      flex: 1,
+      overflow: `scroll`,
+      padding: `50px`,
+      children: [
+        x({
+          children: `count: ${this.state.count}`,
+        }, {
+          onClick: () => this.setState({ count: this.state.count + 1 })
+        }),
+        x({
+          alignItems: `center`,
+          fontSize: `20px`,
+          children: `Todos`
+        }),
+        x({
+          marginTop: `15px`,
+          flexDirection: `row`,
+          children: [
+            Input({
+              flex: `1`,
+              border: `1px solid gray`
+            }, { value: newTodoText, onChange: (e) => { newTodoText = e.target.value; rerender(); } }),
+            x({
+              tag: `button`,
+              marginLeft: `10px`,
+              children: `add todo`,
+            }, {
+              onClick: () => {
+                todos.push({ id: todos.length, text: newTodoText, creationTime: Math.random(), count: 0 });
+                newTodoText = ``;
+                rerender();
+              }
+            })
+          ]
+        }),
+        x({
+          marginTop: `15px`,
+          keyed: true,
+          children: todos.map((todo, i) => x({ tag: Todo }, { todo: todo, key: todo.id }))
+        }),
+        x({
+          marginTop: `10px`,
+          children: [
+            x({
+              children: [
+                x({ children: `sort by creation time` }),
+                x({
+                  children: todos.slice().sort((a, b) => a.creationTime - b.creationTime).map(todo => x({ tag: Todo }, { todo: todo, key: todo.id }))
+                })
+              ]
+            })
+          ]
+        })
+      ]
+    });
+  }
+}
 
 let rerender = () => {
   ReactDOM.render(React.createElement(App), document.body.firstElementChild);
